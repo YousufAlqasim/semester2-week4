2c4e71b92096605526d3a2ec09042db2ba71d82c
Yousuf Alqasim <zwgf0500@leeds.ac.uk>
2026-02-17 10:10:04 +0000

    s

--- session-1/task1/dot_product.c
+++ session-1/task1/dot_product.c
@@ -9,6 +9,11 @@
     float a[]={ 1.0,1.0,1.0,1.0,1.0 };
     float b[]={ 2.0,3.0,4.0,5.0,6.0 };
     float d;
+    
+    for (int i=0; i<5; ++i){
+      d += a[i]*b[i]; 
+    }
+    printf("Dot product = %.2f\n", d);
 
     /*
     Code to compute the dot product
