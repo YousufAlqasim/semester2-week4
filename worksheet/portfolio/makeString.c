a36a92ac05ee4a76bc582a796f1fd15bc3706d3a
Yousuf Alqasim <zwgf0500@leeds.ac.uk>
2026-02-28 23:12:39 +0000

    Refactor loop variable declaration in makeString.c
    Fix variable declaration and improve loop structure.

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -11,12 +11,12 @@ int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
         char buffer[50] = "";
-    for (int i = 1; i < argc; i++) {
+        int i;
+    for (i = 1; i < argc; i++) {
         strcat(buffer, argv[i]);
-
     // add '-' between arguments
         if (i < argc - 1) {
-            strcat(buffer, "-");}
+            strcat(buffer, "-");}}
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
     return 0;
